f5a42445bbd3b6e311d464bc34c41ceefdf03adf
mpietron <mpietron@gmail.com>
2020-09-23 00:54:59 +0200

    Jeszcze lepiej
    Edit triangle.ts

--- src/triangle.ts
+++ src/triangle.ts
@@ -1,10 +1,3 @@
-class CustomError extends Error {
-  constructor(m: string) {
-    super(m);
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-
 /** 
 
 Mieszanie wyjątków z innymi typami to antywzorzec!!! 
@@ -18,12 +11,13 @@ const isRectangularTriangle = (
 ): boolean | never => {
   const [a, b, c]: number[] = [_a, _b, _c].sort((a, b) => a - b);
   if (a <= 0) {
-    throw new CustomError("Line segments must be greater than 0");
+    throw new Error("Line segments must be greater than 0");
   } else if (a + b <= c) {
-    throw new CustomError("Can't build a triangle from the indicated sections");
+    throw new Error("Can't build a triangle from the indicated sections");
   } else {
     return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2) ? true : false;
   }
 };
 
-export { isRectangularTriangle, CustomError };
+export { isRectangularTriangle };
+
