666a10e3f0223437b1798e134d529e459646f948
CODEMO <dev@codemo.pl>
2020-09-23 00:16:49 +0200

    Fix

--- src/triangle.spec.ts
+++ src/triangle.spec.ts
@@ -1,54 +1,21 @@
-import {
-    isRectangularTriangle
-} from './triangle';
+import { isRectangularTriangle, CustomError } from "./triangle";
 
-// it('numbers are more than 0', () => {
-//     expect(isRectangularTriangle(3, 4, 5)).toBe(true)
-// })
+describe("Test throw errors", () => {
+  test("Line segments must be greater than 0", () => {
+    expect(() => isRectangularTriangle(-1, 2, 3)).toThrow(Error);
+  });
 
+  test("Can't build a triangle from the indicated sections", () => {
+    expect(() => isRectangularTriangle(1, 2, 3)).toThrow(Error);
+  });
+});
 
-// describe('function rectangular triangle', () => {
-//     const correctNumbers = [[3, 4, 5], [5, 4, 3], [4, 3, 5], [4, 5, 3], [5, 12, 13], [13, 12, 5], [12, 13, 5], [12, 5, 13]];
+describe("Test triangle", () => {
+  test("Test of true", () => {
+    expect(isRectangularTriangle(3, 4, 5)).toBe(true);
+  });
 
-//     test.each(correctNumbers)('numbers are correct', (x1, x2, x3) => {
-//         expect(isRectangularTriangle(x1, x2, x3)).toBe(true);
-//     })
-
-//     const wrongNumbers = [[5, 7, 9], [9, 7, 5], [7, 5, 9], [9, 5, 7], [9, 16, 25], [25, 16, 9], [16, 9, 25], [25, 9, 16]];
-
-//     test('type error', () => {
-//         expect(isRectangularTriangle).toThrow(`can't build rectangular triangle`)
-//     })
-
-//     test.each(wrongNumbers)('numbers are wrong and throw error', (a, b, c,) => {
-//         expect(() => { isRectangularTriangle(a, b, c) }).toThrow(`can't build rectangular triangle`);
-//     })
-// });
-
-
-describe('function rectangular works correctly', () => {
-    const correctNumbers = [[3, 4, 5], [5, 4, 3], [4, 3, 5], [4, 5, 3], [5, 12, 13], [13, 12, 5], [12, 13, 5], [12, 5, 13]];
-
-    test.each(correctNumbers)('po przekazaniu numerów które tworzą trójkąt prostokątny zwraca true', (x1, x2, x3) => {
-        expect(isRectangularTriangle(x1, x2, x3)).toBe(true);
-    });
-
-    const wrongNumbers = [[5, 7, 9], [9, 7, 5], [7, 5, 9], [9, 5, 7], [9, 16, 25], [25, 16, 9], [16, 9, 25], [25, 9, 16]];
-
-    test.each(wrongNumbers)('po przekazaniu do funkcji numerów które tworzą trójkąt nieprostkątny zwraca false', (x1, x2, x3) => {
-        expect(isRectangularTriangle(x1, x2, x3)).toBe(false);
-    });
-
-})
-
-describe('function rectangular correctly handles error', () => {
-    const negativeNumber2 = [[1, 2, 3], [3, 2, 1]]
-
-    // negativeNumber2.forEach(([x1, x2, x3]) => {
-    // expect(isRectangularTriangle(x1, x2, x3)).toBe(false);
-    // })
-
-    negativeNumber2.forEach(([x1, x2, x3]) => {
-        expect(() => { isRectangularTriangle(x1, x2, x3) }).toThrow('is wrong args')
-    })
+  test("Test of false", () => {
+    expect(isRectangularTriangle(6, 7, 8)).toBe(false);
+  });
 });
--- src/triangle.ts
+++ src/triangle.ts
@@ -1,34 +1,29 @@
-// "2) Utwórz funkcję, która jako argument przyjmie trzy liczby. Funkcja powinna zwrócić true jeśli z odcinków o długości przekazanych w argumentach
-// można zbudować trójkąt prostokątny, lub informację że z podanych długości nie można utworzyć trójkąta prostokątnego "
-
-export const isRectangularTriangle = (x1: number, x2: number, x3: number): boolean => {
-
-  const [a, b, c]: number[] = [x1, x2, x3].sort((_a, _b) => _a - _b);
-  const areAnyNumbersLessAndGreaterThan0 = a <= 0;
-  if (areAnyNumbersLessAndGreaterThan0) throw new Error(`all numbers must be greater than 0`);
-
-  // const triangleCanBeBuild = a + b > c;
-  if (a + b <= c) {
-    throw new Error('is wrong args')
+class CustomError extends Error {
+  constructor(m: string) {
+    super(m);
+    Error.captureStackTrace(this, this.constructor);
   }
-  return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2) ? true : false;
+}
 
+/** 
+
+Mieszanie wyjątków z innymi typami to antywzorzec!!! 
+Funkcja musi wtedy zwracać konkretny typ lub never
+ 
+*/
+const isRectangularTriangle = (
+  _a: number,
+  _b: number,
+  _c: number
+): boolean | never => {
+  const [a, b, c]: number[] = [_a, _b, _c].sort((a, b) => a - b);
+  if (a <= 0) {
+    throw new CustomError("Line segments must be greater than 0");
+  } else if (a + b <= c) {
+    throw new CustomError("Can't build a triangle from the indicated sections");
+  } else {
+    return Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2) ? true : false;
+  }
 };
 
-
-// test:
-//wywalic true jesli jest ok
-// wywali false jest nie jest ok
-// wywalic error jesli nie przejdzie
-//sprawdzic czy przejda ponizej 0 cyfry
-//sprawdzic czy jak dam parametr 3,4,6 zbuduje trojkat i wywali err
-
-console.log(isRectangularTriangle(3, 2, 1))
-// console.log(isRectangularTriangle(1, 2, 3))
-
-// console.log(isRectangularTriangle(3, 5, 1))
-
-// console.log(isRectangularTriangle(3, 8, 1))
-// console.log(isRectangularTriangle(11, 1, 3))
-// console.log(isRectangularTriangle(-2, -3, -4))
-// console.log(isRectangularTriangle(-5, -7, -9))
+export { isRectangularTriangle, CustomError };
