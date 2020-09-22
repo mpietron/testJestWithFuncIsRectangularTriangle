888d6f52312e2c7c72564513093643fd03e53410
mpietron <mpietron@gmail.com>
2020-09-23 00:53:37 +0200

    Jeszcze lepiej
    triangle.spec.ts

--- src/triangle.spec.ts
+++ src/triangle.spec.ts
@@ -1,21 +1,49 @@
-import { isRectangularTriangle, CustomError } from "./triangle";
+import { isRectangularTriangle } from "./triangle";
 
 describe("Test throw errors", () => {
-  test("Line segments must be greater than 0", () => {
-    expect(() => isRectangularTriangle(-1, 2, 3)).toThrow(Error);
-  });
+  let incorrectNumbers = [
+    [-1, 2, 3],
+    [2, -1, 3],
+    [2, 3, -1],
+    [-1, -2, 3],
+    [1, -2, -3],
+  ];
 
-  test("Can't build a triangle from the indicated sections", () => {
-    expect(() => isRectangularTriangle(1, 2, 3)).toThrow(Error);
-  });
+  test.each(incorrectNumbers)(
+    `Line segments must be greater than 0 (a: %i, b: %i, c: %i)`,
+    (a, b, c) => {
+      expect(() => isRectangularTriangle(a, b, c)).toThrow(Error);
+    }
+  );
+  incorrectNumbers = [
+    [1, 2, 3],
+    [2, 3, 1],
+    [3, 2, 1],
+  ];
+
+  test.each(incorrectNumbers)(
+    `Can't build a triangle from the indicated sections (a: %i, b: %i, c: %i)`,
+    (a, b, c) => {
+      expect(() => isRectangularTriangle(a, b, c)).toThrow(Error);
+    }
+  );
 });
 
 describe("Test triangle", () => {
-  test("Test of true", () => {
-    expect(isRectangularTriangle(3, 4, 5)).toBe(true);
+  let correctNumbers = [[3, 4, 5]];
+  test.each(correctNumbers)(`Test of true (a: %i, b: %i, c: %i)`, (a, b, c) => {
+    expect(isRectangularTriangle(a, b, c)).toBe(true);
   });
-
-  test("Test of false", () => {
-    expect(isRectangularTriangle(6, 7, 8)).toBe(false);
-  });
+  correctNumbers = [
+    [6, 7, 8],
+    [7, 8, 6],
+    [8, 7, 6],
+  ];
+  test.each(correctNumbers)(
+    `Test of false (a: %i, b: %i, c: %i)`,
+    (a, b, c) => {
+      expect(isRectangularTriangle(a, b, c)).toBe(false);
+    }
+  );
 });
+
